c2ce8a8f8e642bb7053fde46b574fb6e245c9fa7
ElMarioBros <79614904+ElMarioBros@users.noreply.github.com>
2021-12-07 01:17:44 -0700

    mexicali

---
+++ mexicali.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <style>
+            /*body {
+                background-image: url("landing.png");
+            }*/
+        </style>
+        <img src="mexicali.png" width="100%" alt="caborca">
+    </body>
+</html>
